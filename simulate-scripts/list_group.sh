4be0eb5c9c0c8ab03f55d6e00c7b4e0b4a0d5fcf
Newbie <adsl53102@gmail.com>
2025-07-25 20:48:47 +0800

    chore(docker-compose): link fiware-backend to odoo project network
    - Update docker-compose.yml to use odoo_net for integration with Odoo project
    
    test(simulate-scripts): add list_group.sh for testing purposes

---
+++ simulate-scripts/list_group.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# =================================================================
+# Script to List all provisioned Service Groups in the IoT Agent
+# =================================================================
+
+source ./config.sh
+
+echo "Listing all existing Service Groups for service [${FIWARE_SERVICE}]..."
+echo "------------------------------------------------------------------"
+
+curl -s -L -X GET "http://${HOST}:${IOTA_NORTH_PORT}/iot/services" \
+  -H "${HEADER_FIWARE_SERVICE}" \
+  -H "${HEADER_FIWARE_SERVICEPATH}" | jq
